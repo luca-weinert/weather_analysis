14e908273e7524483a46d313e68b8668b6dd8385
Luca Weinert <lweinert@igus.net>
2024-02-20 12:17:39 +0100

    feat: Add a method to read from a CSV file and parse the data to weatherData object

--- Program.cs
+++ Program.cs
@@ -1,3 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using weather_analysis;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    public static List<WeatherDate> ReadCSV()
+    {
+        string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+            @"weatherData.csv");
+        var reader = new StreamReader(path);
+        List<WeatherDate> weatherDates = new List<WeatherDate>();
+        
+        string line;
+        // ignore the first line (header)
+        reader.ReadLine();
+        
+        while ((line = reader.ReadLine()) != null)
+        {
+            string[] WheatherData = line.Split(",");
+            
+                Console.WriteLine(WheatherData);
+                
+                if (WheatherData.Length == 14)
+                {
+                    WeatherDate weatherDate = new WeatherDate()
+                    {
+                        MeasuringStationNumber = WheatherData[0],
+                        SunshineHours = WheatherData[10],
+                        Temperature = WheatherData[5],
+                        RainAmount = "test"
+                    };
+                    weatherDates.Add(weatherDate);
+                }
+        }
+            
+        foreach (WeatherDate weatherDate in weatherDates)
+        {
+            Console.WriteLine($"MeasuringStationNumber: {weatherDate.MeasuringStationNumber}, SunshineHours: {weatherDate.SunshineHours}, Temperature: {weatherDate.Temperature}, RainAmount: {weatherDate.RainAmount}");
+        }
+
+        return weatherDates;
+    }
+
+    public static string GetHighestTemperature(List<WeatherDate> weatherDates)
+    {
+
+        return "test";
+    }
+    
+    public static void Main(string[] args)
+    {
+        var weatherDates = ReadCSV();
+        var highestTemperature =  GetHighestTemperature(weatherDates);
+    }
+}
---
+++ WeatherDate.cs
@@ -0,0 +1,9 @@
+﻿namespace weather_analysis;
+
+public class WeatherDate
+{
+    public string MeasuringStationNumber { get; set; }
+    public string SunshineHours { get; set; }
+    public string RainAmount { get; set; }
+    public string Temperature { get; set; }
+}
