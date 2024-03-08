f5ad59b8359abd4617df0d93ebe7e95e6b735886
Luca Weinert <lucaweinert8@gmail.com>
2024-03-08 13:39:31 +0100

    feat: impl. fa

--- Program.cs
+++ Program.cs
@@ -22,15 +22,26 @@ class Program
                 
                 if (WeatherData.Length == 14)
                 {
-                    var weatherDate = new WeatherDate()
+                    try
                     {
-                        MeasuringStationNumber = Convert.ToInt32(WeatherData[0]),
-                        DateOfMeasurement = WeatherData[1],
-                        SunshineHours = float.Parse(WeatherData[10],CultureInfo.InvariantCulture.NumberFormat),
-                        Temperature = float.Parse(WeatherData[5], CultureInfo.InvariantCulture.NumberFormat),
-                        RainAmount = float.Parse(WeatherData[12], CultureInfo.InvariantCulture.NumberFormat),
-                    };
-                    weatherDates.Add(weatherDate);
+                        var weatherDate = new WeatherDate()
+                        {
+                            MeasuringStationNumber = Convert.ToInt32(WeatherData[0]),
+                            DateOfMeasurement = WeatherData[1],
+                            SunshineHours = float.Parse(WeatherData[10],CultureInfo.InvariantCulture.NumberFormat),
+                            Temperature = float.Parse(WeatherData[5], CultureInfo.InvariantCulture.NumberFormat),
+                            RainAmount = float.Parse(WeatherData[12], CultureInfo.InvariantCulture.NumberFormat),
+                        };
+                        weatherDates.Add(weatherDate);
+                    }
+                    catch (FormatException exception)
+                    {
+                        Console.WriteLine($"error parsing line: {line}. Exception: {exception.Message}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Incomplete data line (length {WeatherData.Length}): line: {line}");
                 }
         }
             
@@ -42,16 +53,16 @@ class Program
         return weatherDates;
     }
 
-    public static (int, string, double) GetHighestTemperature(List<WeatherDate> weatherDates)
+    public static (int?, string?, double?) GetHighestTemperature(List<WeatherDate> weatherDates)
     {
-        int NumberOfMeasuringStationForHighestTemperature = 0;
-        string DateOfHighestTemperature = "";
-        double HighestTemperature = 0;
+        int ?NumberOfMeasuringStationForHighestTemperature = 0;
+        string ?DateOfHighestTemperature = "";
+        double ?HighestTemperature = 0;
         
         
         foreach (var weatherDate in weatherDates)
         {
-            double currentTemperature = weatherDate.Temperature;
+            double ?currentTemperature = weatherDate.Temperature;
             if (currentTemperature > HighestTemperature)
             {
                 NumberOfMeasuringStationForHighestTemperature = weatherDate.MeasuringStationNumber;
@@ -61,11 +72,27 @@ class Program
         }
         return (NumberOfMeasuringStationForHighestTemperature, DateOfHighestTemperature, HighestTemperature);
     }
+
+    public static void GetAverageTemperature(List<WeatherDate> weatherData)
+    {
+        // weatherData[0].Temperature; 
+
+        double ?TemperatureSum = 0; 
+        
+        foreach (var weatherDate in weatherData)
+        {
+            TemperatureSum = TemperatureSum + weatherDate.Temperature;
+        }
+
+        double ?TemperatureAverage = TemperatureSum / weatherData.Count; 
+        Console.WriteLine($"the Temperature Average is: {TemperatureAverage}");
+    }
     
     public static void Main(string[] args)
     {
-        var weatherDates = ReadCSV();
-        var (NumberOfMeasuringStation, DateOfMeasuring, Temperature) = GetHighestTemperature(weatherDates);
+        var weatherData = ReadCSV();
+        var (NumberOfMeasuringStation, DateOfMeasuring, Temperature) = GetHighestTemperature(weatherData);
         Console.WriteLine($"the highest temperature ist {Temperature} measured from the station {NumberOfMeasuringStation} on {DateOfMeasuring}");
+        GetAverageTemperature(weatherData);
     }
 }
--- WeatherDate.cs
+++ WeatherDate.cs
@@ -2,9 +2,9 @@
 
 public class WeatherDate
 {
-    public int MeasuringStationNumber { get; set; }
-    public string DateOfMeasurement { get; set; }
-    public float SunshineHours { get; set; }
-    public float RainAmount { get; set; }
-    public float Temperature { get; set; }
+    public int ?MeasuringStationNumber{ get; set; }
+    public string ?DateOfMeasurement { get; set; }
+    public float ?SunshineHours { get; set; }
+    public float ?RainAmount { get; set; }
+    public float ?Temperature { get; set; }
 }
