3c42f4f5f2f7d12943d5020dc04646ae39f97d62
Luca Weinert <lweinert@igus.net>
2024-03-12 14:18:54 +0100

    feat: implement class for CSVReader

---
+++ CSVReader.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+
+namespace weather_analysis;
+
+public class CSVReader
+{
+    public CSVReader()
+    {
+        
+    }
+    
+    public List<Measurement> ReadCSV(string fileName)
+    {
+        string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+        var reader = new StreamReader(path);
+        List<Measurement> measurements = new List<Measurement>();
+        
+        string line;
+        // ignore the first line (header)
+        reader.ReadLine();
+        
+        while ((line = reader.ReadLine()) != null)
+        {
+            string[] csvLineValues = line.Split(",");
+            
+                Console.WriteLine(csvLineValues);
+                
+                if (csvLineValues.Length == 14)
+                {
+                    try
+                    {
+                        var measurement = new Measurement()
+                        {
+                            MeasuringStationNumber = Convert.ToInt32(csvLineValues[0]),
+                            DateOfMeasurement = csvLineValues[1],
+                            SunshineHours = float.Parse(csvLineValues[10],CultureInfo.InvariantCulture.NumberFormat),
+                            Temperature = float.Parse(csvLineValues[5], CultureInfo.InvariantCulture.NumberFormat),
+                            RainAmount = float.Parse(csvLineValues[12], CultureInfo.InvariantCulture.NumberFormat),
+                        };
+                        measurements.Add(measurement);
+                    }
+                    catch (FormatException exception)
+                    {
+                        Console.WriteLine($"error parsing line: {line}. Exception: {exception.Message}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Incomplete data line (length {csvLineValues.Length}): line: {line}");
+                }
+        }
+            
+        foreach (var measurement in measurements)
+        {
+            Console.WriteLine($"MeasuringStationNumber: {measurement.MeasuringStationNumber} | DateOfMeasurement: {measurement.DateOfMeasurement} | SunshineHours: {measurement.SunshineHours} | Temperature: {measurement.Temperature} | RainAmount: {measurement.RainAmount}");
+        }
+
+        return measurements;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,62 +1,14 @@
-﻿using System.Globalization;
-using weather_analysis;
+﻿using weather_analysis;
 
 class Program
 {
-    public static List<Measurement> ReadCSV()
-    {
-        string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
-            @"weatherData.csv");
-        var reader = new StreamReader(path);
-        List<Measurement> measurements = new List<Measurement>();
-        
-        string line;
-        // ignore the first line (header)
-        reader.ReadLine();
-        
-        while ((line = reader.ReadLine()) != null)
-        {
-            string[] csvLineValues = line.Split(",");
-            
-                Console.WriteLine(csvLineValues);
-                
-                if (csvLineValues.Length == 14)
-                {
-                    try
-                    {
-                        var measurement = new Measurement()
-                        {
-                            MeasuringStationNumber = Convert.ToInt32(csvLineValues[0]),
-                            DateOfMeasurement = csvLineValues[1],
-                            SunshineHours = float.Parse(csvLineValues[10],CultureInfo.InvariantCulture.NumberFormat),
-                            Temperature = float.Parse(csvLineValues[5], CultureInfo.InvariantCulture.NumberFormat),
-                            RainAmount = float.Parse(csvLineValues[12], CultureInfo.InvariantCulture.NumberFormat),
-                        };
-                        measurements.Add(measurement);
-                    }
-                    catch (FormatException exception)
-                    {
-                        Console.WriteLine($"error parsing line: {line}. Exception: {exception.Message}");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"Incomplete data line (length {csvLineValues.Length}): line: {line}");
-                }
-        }
-            
-        foreach (var measurement in measurements)
-        {
-            Console.WriteLine($"MeasuringStationNumber: {measurement.MeasuringStationNumber} | DateOfMeasurement: {measurement.DateOfMeasurement} | SunshineHours: {measurement.SunshineHours} | Temperature: {measurement.Temperature} | RainAmount: {measurement.RainAmount}");
-        }
 
-        return measurements;
-    }
     
     public static void Main(string[] args)
     {
+        var CSVReader = new CSVReader();
         var MeasurementController = new MeasurementController();
-        MeasurementController.Measurements = ReadCSV();
+        MeasurementController.Measurements = CSVReader.ReadCSV("weatherData.csv");
         var (NumberOfMeasuringStation, DateOfMeasuring, Temperature) = MeasurementController.GetHighestTemperature();
         Console.WriteLine($"the highest temperature ist {Temperature} measured from the station {NumberOfMeasuringStation} on {DateOfMeasuring}");
         /*GetAverageTemperature(weatherData);*/
