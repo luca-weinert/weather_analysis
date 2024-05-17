87f830d97034c386a1961f51d5bbf586e91c71f5
Luca Weinert <lucaweinert8@gmail.com>
2024-05-17 14:34:39 +0200

    feat: remove some classe

--- Measurement.cs
+++ Measurement.cs
@@ -2,9 +2,4 @@
 
 public class Measurement
 {
-    public int MeasuringStationNumber{ get; set; }
-    public string DateOfMeasurement { get; set; }
-    public float SunshineHours { get; set; }
-    public float RainAmount { get; set; }
-    public float Temperature { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,63 +1,6 @@
-﻿using System.Globalization;
-using weather_analysis;
-
-class Program
+﻿class Program
 {
-    public static List<Measurement> ReadCSV()
-    {
-        string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
-            @"weatherData.csv");
-        var reader = new StreamReader(path);
-        List<Measurement> weatherDates = new List<Measurement>();
-        
-        string line;
-        // ignore the first line (header)
-        reader.ReadLine();
-        
-        while ((line = reader.ReadLine()) != null)
-        {
-            string[] WeatherData = line.Split(",");
-                if (WeatherData.Length == 14)
-                {
-                    try
-                    {
-                        var weatherDate = new Measurement()
-                        {
-                            MeasuringStationNumber = Convert.ToInt32(WeatherData[0]),
-                            DateOfMeasurement = WeatherData[1],
-                            SunshineHours = float.Parse(WeatherData[10],CultureInfo.InvariantCulture.NumberFormat),
-                            Temperature = float.Parse(WeatherData[5], CultureInfo.InvariantCulture.NumberFormat),
-                            RainAmount = float.Parse(WeatherData[12], CultureInfo.InvariantCulture.NumberFormat),
-                        };
-                        weatherDates.Add(weatherDate);
-                    }
-                    catch (FormatException exception)
-                    {
-                        Console.WriteLine($"error parsing line: {line}. Exception: {exception.Message}");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"Incomplete data line (length {WeatherData.Length}): line: {line}");
-                }
-        }
-            
-        foreach (var weatherDate in weatherDates)
-        {
-            Console.WriteLine($"MeasuringStationNumber: {weatherDate.MeasuringStationNumber} | DateOfMeasurement: {weatherDate.DateOfMeasurement} | SunshineHours: {weatherDate.SunshineHours} | Temperature: {weatherDate.Temperature} | RainAmount: {weatherDate.RainAmount}");
-        }
-
-        return weatherDates;
-    }
-
     public static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the Weather analysis");
-        Console.WriteLine("what do you want to do?");
-        Console.ReadKey();
-                    
-        var CSVReader = new CSVReader();
-        var MeasurementController = new MeasurementController();
-
     }
 }
