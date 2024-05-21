c2436653e49aa459645fc7a6bec66bc791250026
Luca Weinert <lweinert@igus.net>
2024-05-21 14:28:44 +0200

    feat: implement basic selection sort algorithm

--- MeasurementList.cs
+++ MeasurementList.cs
@@ -6,12 +6,44 @@ public class MeasurementList
 
     public void Sort(SortBy sortBy, SortingWith sortingWith)
     {
-        
+        switch(sortingWith)
+        {
+            case SortingWith.BubbleSort:
+            {
+                BubbleSort();
+                break;
+            }
+                
+            case SortingWith.SelectionSort:
+            {
+                SelectionSort();
+                break;
+            }
+            default:
+                throw new ArgumentOutOfRangeException(nameof(sortingWith), sortingWith, null);
+        }
     }
 
     private void SelectionSort()
     {
-        
+        var listLength = Measurements.Count;
+
+        for (var i = 0; i < listLength -1 ; i++)
+        {
+            var smallestVal = i;
+
+            for (var j = i + 1; j < listLength; j++)
+            {
+                if (Measurements[j].Temperature < Measurements[smallestVal].Temperature)
+                {
+                    smallestVal = j;
+                }
+            }
+
+            var tempVar = Measurements[smallestVal];
+            Measurements[smallestVal] = Measurements[i];
+            Measurements[i] = tempVar; 
+        }
     }
 
     private void BubbleSort()
--- Program.cs
+++ Program.cs
@@ -6,15 +6,30 @@ class Program
     {
         var measurements = new List<Measurement>
         {
-            new Measurement { Temperature = 12, Date = DateTime.Now, StationNumber = 1 },
-            new Measurement { Temperature = 12, Date = DateTime.Now, StationNumber = 2 },            
-            new Measurement { Temperature = 12, Date = DateTime.Now, StationNumber = 3 }
+            new Measurement { Temperature = 40, Date = DateTime.Now, StationNumber = 1 },
+            new Measurement { Temperature = 1, Date = DateTime.Now, StationNumber = 2 },
+            new Measurement { Temperature = 90, Date = DateTime.Now, StationNumber = 3 },
+            new Measurement { Temperature = 4, Date = DateTime.Now, StationNumber = 3 },
+            new Measurement { Temperature = 2, Date = DateTime.Now, StationNumber = 3 }
         };
 
         var measurementList = new MeasurementList
         {
             Measurements = measurements
         };
-        measurementList.Sort(SortBy.Temperature, SortingWith.SelectionSort);
+        
+        foreach (var measurement in measurementList.Measurements)
+        {
+            Console.WriteLine(measurement.Temperature);
+        }
+        
+       measurementList.Sort(SortBy.Temperature, SortingWith.SelectionSort);
+
+       Console.WriteLine("------------");
+       
+       foreach (var measurement in measurementList.Measurements)
+       {
+           Console.WriteLine(measurement.Temperature);
+       }
     }
 }
